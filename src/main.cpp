c877df44f8cccc040902f86e31c9617fe753dcc3
heyManNice <3031978990@qq.com>
2025-03-04 00:41:23 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,18 @@
 #include "EasyWin32.hpp"
+#include <string>
 
 int main(int argc, char* argv[]){
-    auto window = new EWindow({.title="Test",.width = 1000, .height = 500});
-    auto window2 = new EWindow({.title="Test2",.width = 1000,.height = 1000});
-
-    window->on(WM_DESTROY, ECallback{
-        EApplication->alert("exit...");
+    auto window = new EWindow();
+    window->setTitle("Test")
+    ->setSize(1500, 1000)
+    ->setPosition(400,400)
+    ->onDestroy(
         EApplication->quit();
-        return 0;
-    });
-
-    window->on(WM_PAINT, ECallback{
-        HDC hdc;
-        PAINTSTRUCT ps;
-        hdc = BeginPaint(hwnd, &ps);
-        std::string str = "Hello, World!";
-        TextOut(hdc, 0, 0, str.c_str(), str.length());
-        EndPaint(hwnd, &ps);
-        return 0;
-    });
+    )
+    ->onMouseLeftUp(
+        EApplication->alert(std::to_string(EApplication->getDPI()));
+    )
+    ->show();
 
     return 0;
 }
