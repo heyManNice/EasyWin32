310e414f02b30f828a37591a6791f16ca70461b6
heyManNice <3031978990@qq.com>
2025-03-06 22:51:03 +0800

    refactor:EWindow

--- include/EWindow.hpp
+++ include/EWindow.hpp
@@ -1,1 +1,75 @@
 #pragma once
+
+#include "types.hpp"
+#include "EWidget.hpp"
+
+/**
+ * @brief 窗口类
+ * 
+ */
+class EWindow : public EWidget {
+    private:
+        friend class EApplicationSingleton;
+    
+        HWND hwnd;
+        WNDCLASSEXW wcex;
+        static LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+        void initialize();
+        /**
+         * @brief 更新窗口主题色
+         * 
+         */
+        void updateThemeMode();
+    public:
+        EWindow();
+        ~EWindow();
+        /**
+         * @brief 注册事件
+         * 
+         * @param message 事件类型
+         * @param callback 回调函数
+         */
+        EWindow* on(const UINT message, EventEmitterCallback callback);
+        /**
+         * @brief 重绘窗口
+         *
+         */
+        void rePaint();
+        /**
+         * @brief 获取窗口句柄
+         * 
+         */
+        HWND getHWND();
+        /**
+         * @brief 设置窗口主题色
+         * 
+         * 自动更新到窗口
+         * @param theme 
+         */
+        void setThemeMode(ThemeMode theme);
+    
+        /**
+         * @brief 设置标题
+         * 
+         */
+        EWindow* setTitle(const std::wstring& title);
+    
+        /**
+         * @brief 设置宽高
+         * 
+         */
+        EWindow* setSize(const UINT32 width, const UINT32 height);
+    
+        /**
+         * @brief 设置位置
+         *
+         */
+        EWindow* setPosition(const INT32 x, const INT32 y);
+    
+        /**
+         * @brief 显示窗口
+         * 
+         */
+        EWindow* show();
+    };
+    
---
+++ src/EWindow.cpp
@@ -0,0 +1,190 @@
+#include "EWindow.hpp"
+#include "EApplication.hpp"
+
+#include <gdiplus.h>
+#pragma comment(lib, "Gdiplus.lib")
+
+#include <winrt/Windows.UI.ViewManagement.h>
+
+#include <dwmapi.h>
+#pragma comment(lib, "dwmapi.lib")
+
+#include <shellscalingapi.h>
+#pragma comment(lib, "Shcore.lib")
+
+EWindow::EWindow()
+{
+    this->width = 800;
+    this->height = 600;
+    this->initialize();
+    EApplication->addWindow(this);
+}
+EWindow::~EWindow()
+{
+    EApplication->removeWindow(this);
+}
+
+EWindow *EWindow::on(const UINT message, EventEmitterCallback callback)
+{
+    EApplication->eventEmitter.on(this->hwnd, message, callback);
+    switch (message)
+    {
+    case WM_PAINT:
+        // 如果注册WM_PAINT回调后视图立即生效
+        this->rePaint();
+        break;
+    default:
+        break;
+    }
+    return this;
+}
+
+void EWindow::rePaint()
+{
+    InvalidateRect(this->hwnd, NULL, TRUE);
+}
+
+HWND EWindow::getHWND()
+{
+    return this->hwnd;
+}
+
+EWindow *EWindow::setTitle(const std::wstring &title)
+{
+    this->title = title;
+    SetWindowTextW(this->hwnd, title.c_str());
+    return this;
+}
+
+EWindow *EWindow::setSize(const UINT32 width, const UINT32 height)
+{
+    this->width = width;
+    this->height = height;
+    RECT rect;
+    rect.left = 0;
+    rect.top = 0;
+    rect.right = width;
+    rect.bottom = height;
+    AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+    SetWindowPos(this->hwnd, NULL, 0, 0, rect.right - rect.left, rect.bottom - rect.top, SWP_NOMOVE | SWP_NOZORDER);
+    return this;
+}
+
+EWindow *EWindow::setPosition(const INT32 x, const INT32 y)
+{
+    this->x = x;
+    this->y = y;
+    SetWindowPos(this->hwnd, NULL, x, y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+    return this;
+}
+
+EWindow *EWindow::show()
+{
+    ShowWindow(this->hwnd, SW_SHOW);
+    UpdateWindow(this->hwnd);
+    return this;
+}
+
+void EWindow::initialize()
+{
+    // 注册窗口类
+    this->wcex.cbSize = sizeof(WNDCLASSEX);
+    this->wcex.style = CS_HREDRAW | CS_VREDRAW;
+    this->wcex.lpfnWndProc = &EWindow::WndProc;
+    this->wcex.cbClsExtra = 0;
+    this->wcex.cbWndExtra = 0;
+    this->wcex.hInstance = GetModuleHandle(NULL);
+    this->wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    this->wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    this->wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    this->wcex.lpszMenuName = NULL;
+    this->wcex.lpszClassName = this->title.c_str();
+    this->wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+    RegisterClassExW(&this->wcex);
+
+    // 创建窗口
+    EApplication->updateDpiFromMonitor();
+    this->hwnd = CreateWindowExW(NULL, this->title.c_str(), this->title.c_str(), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, this->width, this->height, NULL, NULL, this->wcex.hInstance, NULL);
+    this->updateThemeMode();
+}
+
+void EWindow::updateThemeMode()
+{
+    BOOL isDark = FALSE;
+    switch (this->theme)
+    {
+    case ThemeMode::Light:
+        isDark = FALSE;
+        break;
+    case ThemeMode::Dark:
+        isDark = TRUE;
+        break;
+    default:
+    {
+        auto settings = winrt::Windows::UI::ViewManagement::UISettings();
+        auto clr = settings.GetColorValue(winrt::Windows::UI::ViewManagement::UIColorType::Foreground);
+        isDark = ((5 * clr.G) + (2 * clr.R) + clr.B) > (8 * 128);
+        break;
+    }
+    }
+    // 20 is DWMWA_USE_IMMERSIVE_DARK_MODE
+    DwmSetWindowAttribute(this->hwnd, 20, &isDark, sizeof(isDark));
+    UpdateWindow(this->hwnd);
+}
+
+void EWindow::setThemeMode(ThemeMode theme)
+{
+    this->theme = theme;
+    this->updateThemeMode();
+}
+
+LRESULT CALLBACK EWindow::WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    // 系统主题改变时更新窗口主题色
+    case WM_SETTINGCHANGE:
+    {
+        auto window = EApplication->getEWindowByHWND(hwnd);
+        if (window != nullptr)
+        {
+            window->updateThemeMode();
+        }
+        // 更新dpi
+        EApplication->updateDpiFromMonitor();
+        break;
+    }
+
+    case WM_PAINT:
+    {
+        // 初始化
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        Gdiplus::Graphics graphics(hdc);
+        graphics.SetSmoothingMode(Gdiplus::SmoothingModeAntiAlias);
+        graphics.SetTextRenderingHint(Gdiplus::TextRenderingHintAntiAlias);
+        int dpi = EApplication->getDPI();
+        double scale = (double)dpi / 96;
+        graphics.ScaleTransform(scale, scale);
+
+        // 绘图
+        Gdiplus::Pen pen(Gdiplus::Color(255, 0, 0, 0));
+        pen.SetWidth(10);
+        graphics.DrawRectangle(&pen, 10, 10, 200, 100);
+        graphics.DrawEllipse(&pen, 500, 500, 100, 100);
+        Gdiplus::FontFamily fontFamily(L"HarmonyOS Sans sC");
+        Gdiplus::Font font(&fontFamily, 100, Gdiplus::FontStyleRegular, Gdiplus::UnitPixel);
+        Gdiplus::SolidBrush solidBrush(Gdiplus::Color(255, 0, 0, 0));
+        Gdiplus::PointF pointF(100, 300);
+        graphics.DrawString(L"Hello, GDI+!鸿蒙系统", -1, &font, pointF, &solidBrush);
+
+        // 结束
+        EndPaint(hwnd, &ps);
+        break;
+    }
+
+    default:
+        break;
+    }
+    return EApplication->eventEmitter.emit(hwnd, message, wParam, lParam);
+}
