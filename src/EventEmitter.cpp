23dc63cff3495f3f5ea3db027ef1ab0c929b74ab
heyManNice <3031978990@qq.com>
2025-03-07 13:50:53 +0800

    refactor:支持EventEmitter的事件过程返回值

--- src/EventEmitter.cpp
+++ src/EventEmitter.cpp
@@ -2,13 +2,28 @@
 
 
 LRESULT EventEmitter::emit(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    if(HwndMap.find(hwnd) != HwndMap.end()) {
-        if(HwndMap[hwnd].find(message) != HwndMap[hwnd].end()) {
-            for(auto callback : HwndMap[hwnd][message]) {
-                callback(hwnd, message, wParam, lParam);
-            } 
-        }
+    if( HwndMap.find(hwnd) == HwndMap.end() ||
+        HwndMap[hwnd].find(message) == HwndMap[hwnd].end())
+    {
+        return DefWindowProcW(hwnd, message, wParam, lParam);
     }
+
+    LRESULT callbackResult = 0;
+    LRESULT callbackCount = 0;
+    for(auto callback : HwndMap[hwnd][message]) {
+        callbackCount++;
+        callbackResult += callback(hwnd, message, wParam, lParam);
+    }
+
+    //返回值用于告诉系统是否已处理信息
+    if(message == WM_ERASEBKGND || message == WM_SETCURSOR){
+        //这两个事件比较特殊，需要返回1表示已处理
+        if(callbackResult && callbackCount) return 1;
+    }else{
+        //其他事件需要返回0表示已处理
+        if(!callbackResult && callbackCount) return 0;
+    }
+    //如果用户的回调函数没有通知系统已处理信息，则返回系统默认处理结果
     return DefWindowProcW(hwnd, message, wParam, lParam);
 }
 
