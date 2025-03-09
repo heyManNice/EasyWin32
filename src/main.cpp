a927f3915d0483d3e21b52e4a77c0bb197db92e2
heyManNice <3031978990@qq.com>
2025-03-10 00:22:22 +0800

    test:弱类型测试

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include "main.hpp"
 #include "EasyWin32.hpp"
+#include "var.hpp"
 
 int main(int argc, char* argv[]){
+
+    var str1 = "hello";
+    var str2 = str1 + " world!";
+    var num1 = 300;
+    var str3 = str2 + num1;
+
+    var num2 = "200";
+
+    //验证
+    std::string str = str3;
+    int num = num2;
+
     auto window = new EWindow();
     window->setTitle(L"Test")
     ->setSize(1500, 1000)
